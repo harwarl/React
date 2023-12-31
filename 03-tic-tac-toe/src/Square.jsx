dcf0a63a35fdaee99735f4d1f339de1224899902
harwarl <harwarl87@gmail.com>
2023-12-31 17:24:41 +0100

    feat: tictactoe completed

---
+++ 03-tic-tac-toe/src/Square.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  //   const [value, setValue] = useState(null);
+
+  //   function handleClick() {
+  //     setValue("X");
+  //   }
+
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
