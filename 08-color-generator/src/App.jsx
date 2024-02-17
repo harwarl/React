21ce9e062dc95c05d75536c754d0de0619258e4e
harwarl <harwarl87@gmail.com>
2024-02-18 00:06:55 +0100

    color generator added

--- 08-color-generator/src/App.jsx
+++ 08-color-generator/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Form from './Form';
+import { useState } from 'react';
+import Values from 'values.js';
+import ColorList from './ColorList';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [colors, setColors] = useState(new Values('#f15025').all(10));
 
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10);
+      setColors(newColors);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <Form addColor={addColor} />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ 08-color-generator/src/ColorList.jsx
@@ -0,0 +1,12 @@
+import SingleColor from './SingleColor';
+import { nanoid } from 'nanoid';
+function ColorList({ colors }) {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
+      })}
+    </section>
+  );
+}
+export default ColorList;
